8f7e7c0bf568f5a2a26ba9ad3615ef5539f6f8a4
maegima <andre.maegima@unifesp.br>
2021-07-25 16:50:37 -0300

    Add forms service

---
+++ api/services/forms.py
@@ -0,0 +1,35 @@
+from flask import request
+from api import db
+from api.model.Formulario_Socioeconomico import Form_Socioeconomico
+
+def FormSocio(id):
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=data['pessoa'], qtd_pessoas_familia=data['qtd_pessoas_familia'],
+            pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
+            db.session.add(new_form)
+            db.session.commit()
+
+            return {"message": f"Formulário enviado!"}
+        else:
+            return {"error": "O envio não foi feita no formato esperado"}
+ 
+    elif request.method == 'GET':
+        forms = Form_Socioeconomico.query.filter_by(pessoa=id).all()
+        results = []
+        for form in forms:
+            if form.preenchido:
+                results.append({
+                    "preenchido": form.preenchido,
+                    "nome_rep": form.nome_rep_familia,
+                    "qtd_pessoas": form.qtd_pessoas_familia,
+                    "qtd_criancas": form.qtd_criancas,
+                    "gestante": form.gestante,
+                    "qtd_amamentando": form.qtd_amamentando,
+                    "qtd_criancas_deficiencia": form.qtd_criancas_deficiencia,
+                    "pessoa_amamenta": form.pessoa_amamenta,
+                    "qtd_gestantes": form.qtd_gestantes
+                })
+
+        return {"count": len(results), "users": results}
--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from api.model.Postagem import *
 from api.services.auth import EsqueciSenha, token_required, Login
 from api.model.Formulario_Socioeconomico import *
 from api.services.users import VerifyUsername, HandleUser, Users, Privileges, Bairro
+from api.services.forms import FormSocio
 
 
 
@@ -51,36 +52,7 @@ def handle_user_notificacao(id):
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 @token_required
 def form_socio(id):
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=data['pessoa'], qtd_pessoas_familia=data['qtd_pessoas_familia'],
-            pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
-            db.session.add(new_form)
-            db.session.commit()
-
-            return {"message": f"Formulário enviado!"}
-        else:
-            return {"error": "O envio não foi feita no formato esperado"}
-
-    elif request.method == 'GET':
-        forms = Form_Socioeconomico.query.filter_by(pessoa=id).all()
-        results = []
-        for form in forms:
-            if form.preenchido:
-                results.append({
-                    "preenchido": form.preenchido,
-                    "nome_rep": form.nome_rep_familia,
-                    "qtd_pessoas": form.qtd_pessoas_familia,
-                    "qtd_criancas": form.qtd_criancas,
-                    "gestante": form.gestante,
-                    "qtd_amamentando": form.qtd_amamentando,
-                    "qtd_criancas_deficiencia": form.qtd_criancas_deficiencia,
-                    "pessoa_amamenta": form.pessoa_amamenta,
-                    "qtd_gestantes": form.qtd_gestantes
-                })
-
-        return {"count": len(results), "users": results}
+    return FormSocio(id)
 
 @app.route('/users', methods=['POST', 'GET'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
