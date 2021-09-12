c041804493a945ee11d25555229c01944d71b74e
maegima <andre.maegima@unifesp.br>
2021-09-11 22:26:41 -0300

    Separeted get and post requests

--- api/service/privileges.py
+++ api/service/privileges.py
@@ -1,28 +1,21 @@
 from flask import request
 from api import db
-from api.model.database.users import Privilegio
+from api.model.database.privileges import Privilegio
 
-#TODO: separar POST e GET
-#TODO: remover verificação de método
-#TODO: remover verificação de json POST
-def Privileges():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_privilege = Privilegio(user_type=data['user_type'])
+def GetPrivileges():
+    privileges = Privilegio.query.all()
+    results = [
+        {
+            "user_type": privilege.user_type
+        } for privilege in privileges]
 
-            db.session.add(new_privilege)
-            db.session.commit()
+    return {"count": len(results), "privileges": results, "message": "success"}
 
-            return {"message": f"Privilégio criado com sucesso"}
-        else:
-            return {"error": "A requisição não foi feita no formato esperado"}
+def PostPrivileges(data):
+    data = request.get_json()
+    new_privilege = Privilegio(user_type=data['user_type'])
 
-    elif request.method == 'GET':
-        privileges = Privilegio.query.all()
-        results = [
-            {
-                "user_type": privilege.user_type
-            } for privilege in privileges]
+    db.session.add(new_privilege)
+    db.session.commit()
 
-        return {"count": len(results), "Privileges": results, "message": "success"}
+    return {"message": f"Privilégio criado com sucesso"}
