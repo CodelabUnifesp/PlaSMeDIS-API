c3c1c58f95e47d95396670d8976bbbb89f91b1a7
maegima <andre.maegima@unifesp.br>
2021-12-08 21:06:23 -0300

    Added notification calls response and request models

--- api/controller/notifications.py
+++ api/controller/notifications.py
@@ -2,19 +2,18 @@ from api.util.decorators import required
 from api.service.notifications import GetUserNotification, PutUserNotification
 from api import api
 from flask_restx import Resource, cors
-#import api.model.request.privileges as request
-#import api.model.response.privileges as response
+import api.model.response.notifications as response
 import api.model.response.default as default
 
 notifications = api.namespace('notifications', description="Notifications namespace", decorators=[cors.crossdomain(origin="*")])
 
 @notifications.route("/users/<int:id>/conf")
 class NotificationConf(Resource):
-    @required(response=default.message)
+    @required(response=response.notification_message, token=True)
     def get(self, id):
         return GetUserNotification(id)
     
-    @required(response=default.message, token=True)
+    @required(response=default.message, request=response.notification, token=True)
     def put(self, data, id):
         return PutUserNotification(data, id)
     
