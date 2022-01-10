e332e50d0cc3c4cb2afe81ed1e63e7d86ff1c083
maegima <andre.maegima@unifesp.br>
2022-01-10 15:30:24 -0300

    Added neighborhoods service

--- api/controller/neighborhoods.py
+++ api/controller/neighborhoods.py
@@ -1,6 +1,6 @@
 from flask_cors import cross_origin
 from api.util.decorators import required
-from api.service.users import Bairros
+from api.service.neighborhoods import GetNeighborhoods, PostNeighborhoods
 from api import api
 from flask_restx import Resource
 import api.model.request.users as request
@@ -14,9 +14,9 @@ class User(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=default.message, token=False)
     def get(self):
-        return Bairros()
+        return GetNeighborhoods()
     
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=response.user_create_message, token=False)
     def post(self, data):
-        return Bairros()
+        return PostNeighborhoods(data)
