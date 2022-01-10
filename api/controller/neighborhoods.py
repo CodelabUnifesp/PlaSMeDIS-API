78d5bfdfaa0a6a9871ffb2f4580e928a4e0f0c51
maegima <andre.maegima@unifesp.br>
2022-01-10 15:41:34 -0300

    Documented get /neighborhoods

--- api/controller/neighborhoods.py
+++ api/controller/neighborhoods.py
@@ -4,7 +4,7 @@ from api.service.neighborhoods import GetNeighborhoods, PostNeighborhoods
 from api import api
 from flask_restx import Resource
 import api.model.request.users as request
-import api.model.response.users as response
+import api.model.response.neighborhoods as response
 import api.model.response.default as default
 
 neighborhoods = api.namespace('neighborhoods', description="Neighborhood namespace")
@@ -12,11 +12,11 @@ neighborhoods = api.namespace('neighborhoods', description="Neighborhood namespa
 @neighborhoods.route("")
 class User(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    @required(response=default.message, token=False)
+    @required(response=response.neighborhood_list, token=True)
     def get(self):
         return GetNeighborhoods()
     
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    @required(response=response.user_create_message, token=False)
+    @required(response=default.message, token=False)
     def post(self, data):
         return PostNeighborhoods(data)
